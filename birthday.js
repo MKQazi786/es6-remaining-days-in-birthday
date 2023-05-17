086ef26bea5237863280e6217b5ef07ef9436a5a
Muhammad Khubaib <muhammadkhubaib492@gmail.com>
2023-05-17 08:36:47 +0500

    i ahve face error on js file

--- birthday.js
+++ birthday.js
@@ -7,7 +7,7 @@ function remainingBirthday(event) { {
 
     date = Number(date)
 
-    let currentFullDate = newDate()
+    let currentFullDate = new Date()
 
     let currentMonth = currentFullDate.getMonth()
     currentMonth = Number(currentMonth)
@@ -16,60 +16,123 @@ function remainingBirthday(event) { {
 
     if (month === "January") {
 
-        month2 = 0
+        newMonth = 0
 
     } else if (month = "February") {
 
-        month2 = 1
+        newMonth = 1
 
     } else if (month = "March") {
 
-        month2 = 2
+        newMonth = 2
 
     } else if (month = "April") {
 
-        month2 = 3
+        newMonth = 3
 
     } else if (month = "May") {
 
-        month2 = 4
+        newMonth = 4
 
     } else if (month = "June") {
 
-        month2 = 5
+        newMonth = 5
 
     } else if (month = "July") {
 
-        month2 = 6
+        newMonth = 6
 
     } else if (month = "August") {
 
-        month2 = 7
+        newMonth = 7
 
     } else if (month = "September") {
 
-        month2 = 8
+        newMonth = 8
 
     } else if (month = "October") {
 
-        month2 = 9
+        newMonth = 9
 
     } else if (month = "November") {
 
-        month2 = 10
+        newMonth = 10
 
     } else if (month = "December") {
 
-        month2 = 11
+        newMonth = 11
 
     }
 
-    let months = month2 - currentMonth 
+    let months = newMonth - currentMonth 
 
     let dates = date - currentDate
-    
-    // title = `$(months) months $(dates) dates`
 
-    // document.getElementById("title").innerHTML = title
+    let hour = currentFullDate.getHours()
+
+    let hours = 23 - hour
+
+    let minute = currentFullDate.getMinutes()
+
+    let minutes = minute - 60
+
+    let second = currentFullDate.getSeconds()
+
+    let seconds = 60 - second
+
+    let milliSecond = currentFullDate.getMilliseconds()
+
+    let milliSeconds = 1000 - milliSeconds
+
+
+
+    if (newMonth === 1) {
+        
+        month1 = month
+
+    } else {
+        month1 = months
+    }
+    
+    if (dates === 1) {
+        
+        date1 = day
+
+    } else {
+        date1 = days
+    }
+
+    if (hours === 1) {
+        
+        hour1 = hour
+
+    } else {
+        hour1 = hours
+    }
+
+    if (minutes === 1) {
+        
+        minutes1 = minute
+
+    } else {
+        minutes1 = minutes
+    }
+
+    if (seconds === 1) {
+        
+        seconds1 = seconds
+
+    } else {
+        seconds1 = second
+    }
+
+    
+
+    
+    title = `$(months) $(month1) ,<hr/> $(dates) $(date1) ,<hr/> $(hours) $(hour1) ,<hr/> 
+    $(minutes) $(minute1) ,<hr/> $(seconds) $(second1) ,<hr/> $(milliSeconds) `
+
+    console.log(title)
+    // document.querySelector('#title').innerHTML = title
 }
 
